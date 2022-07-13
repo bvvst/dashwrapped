c32ec62b98333c1026efc2dc1c2860b726b7afe3
luke <lukeimbing@gmail.com>
2022-07-13 12:02:32 -0400

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className='text-red-500'>
+      hi
+    </div>
+  )
+}
+
+export default Home
